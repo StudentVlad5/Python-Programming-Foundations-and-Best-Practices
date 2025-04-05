4f34d251386b9cdac90e4d6238502a93a6f569fa
Vlad <vlad_np@ukr.net>
2025-04-05 19:42:59 +0200

    add encoding

--- services/commands_logs.py
+++ services/commands_logs.py
@@ -6,7 +6,7 @@ def log_request(func):
     def wrapper(*args, **kwargs):
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         log_msg = f"{timestamp} | Function: {func.__name__} | Args: {args} \n"
-        with open("log_file.txt", "a") as log_file:
+        with open("log_file.txt", "a", encoding="utf-8") as log_file:
             log_file.write(log_msg)
         
         return func(*args, **kwargs)
