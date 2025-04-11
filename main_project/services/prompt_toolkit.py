dad11f9779c0fcaf0d6de7b10d367fea023c0ae6
Vlad <vlad_np@ukr.net>
2025-04-11 10:03:23 +0200

    add file for start project

--- main_project/services/prompt_toolkit.py
+++ main_project/services/prompt_toolkit.py
@@ -8,9 +8,14 @@ console=Console()
 @log_request
 def handle_user_input(user_input):
     command_keys = command_d_keys()
-    if user_input.split()[0] in command_keys:
+    if not user_input.strip():
+        console.print(Text("No input detected. Please enter a command.", style='yellow'))
+        return None
+
+    command = user_input.split()[0]
+    if command in command_keys:
         console.print(Text(f"Implementation: {user_input}", style='cyan'))
         return user_input
     else:
         console.print(Text("Unknown command. Try again.", style='red'))
-        return None  
+        return None
