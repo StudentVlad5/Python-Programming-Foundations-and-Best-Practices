199631ae0a43f1855a4b4cfc90aa000bb8972138
dvankevich <dvankevich@gmail.com>
2025-04-13 19:45:09 +0300

    fix import path in cli.py

--- main_project/modules/cli.py
+++ main_project/modules/cli.py
@@ -48,14 +48,14 @@ Note:
 """
 import click
 import contextlib
-from modules.AddressBook_m.addressbook_m import AddressBook
+from main_project.modules.AddressBook_m.addressbook_m import AddressBook
 #from modules.Notes_m.note_m import Note
-from modules.Common_m.CONSTANT import filename, filenameNotes
+from main_project.modules.Common_m.CONSTANT import filename, filenameNotes
 #from modules.Common_m.dictionary import command_d
-from services.file_manager import load_data, save_data
+from main_project.services.file_manager import load_data, save_data
 
-from services.address_book_manager import hello, add_contact, add_phone, add_birthday, show_all_contacts, show_phone, show_birthday, delete_contact, add_email, show_email, add_address, show_address, birthdays_all, birthdays, edit_address, edit_birthday, edit_email, edit_phone, delete_address, delete_birthday, delete_email, delete_phone, show_contact
-from services.note_manager import add_note, show_all_notes, add_tag, delete_tag, show_note, delete_note, search_tag, search_message
+from main_project.services.address_book_manager import hello, add_contact, add_phone, add_birthday, show_all_contacts, show_phone, show_birthday, delete_contact, add_email, show_email, add_address, show_address, birthdays_all, birthdays, edit_address, edit_birthday, edit_email, edit_phone, delete_address, delete_birthday, delete_email, delete_phone, show_contact
+from main_project.services.note_manager import add_note, show_all_notes, add_tag, delete_tag, show_note, delete_note, search_tag, search_message
 
 book = AddressBook()
 # notes = Note()
